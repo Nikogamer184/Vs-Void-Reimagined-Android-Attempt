134b93f14c782d58941c7d86edb7276bba134e5a
Nikogamer184 <98852651+Nikogamer184@users.noreply.github.com>
2022-11-20 17:37:58 -0500

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1197,7 +1197,7 @@ class PlayState extends MusicBeatState
 		// SONG SPECIFIC SCRIPTS
 		#if LUA_ALLOWED
 		var filesPushed:Array<String> = [];
-		var foldersToCheck:Array<String> = ['assets/data/' + Paths.formatToSongPath(SONG.song) + '/')];
+		var foldersToCheck:Array<String> = ['assets/data/' + Paths.formatToSongPath(SONG.song) + '/'];
 
 		for (folder in foldersToCheck)
 		{
